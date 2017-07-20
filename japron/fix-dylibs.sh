9d133d3d0d7f12c9d14e8966a69c6fbeb5b51262
Tianhan Lu <tianhan.lu@colorado.edu>
2017-07-20 16:54:23 -0600

    fix dylib suffix

--- japron/fix-dylibs.sh
+++ japron/fix-dylibs.sh
@@ -1,3 +1,6 @@
-for f in $out/lib/*.so; do
-    mv -- "$f" "${f%.so}.dylib"
-done
+#for f in $out/lib/*.so; do
+#    mv -- "$f" "${f%.so}.dylib"
+#done
+
+mv $out/lib/libjapron.so $out/lib/libjapron.dylib
+cp $out/lib/libjgmp.so $out/lib/libjgmp.dylib
